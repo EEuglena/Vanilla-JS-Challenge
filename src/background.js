8085843cefbd5a552aefb5e3d206b933e47c89d6
EEuglena <halfmoon51525@gmail.com>
2023-07-21 13:42:46 +0900

    Path edit

--- src/background.js
+++ src/background.js
@@ -4,7 +4,7 @@ const background = document.querySelector(".background img");
 
 const loadBackground = () => {
 	const option = Math.floor(Math.random() * NUMBER_OF_IMAGES);
-	const path = `/src/images/${option}.jpg`;
+	const path = `./src/images/${option}.jpg`;
 	background["src"] = path;
 };
 
